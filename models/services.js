5667f94f9e3976e6955dd68666c7cdabd49174b8
GamingHazard <bb466882@gmail.com>
2024-11-12 03:26:15 -0800

    update

--- models/services.js
+++ models/services.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose");
 
-const ServicesSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    componentName: { type: String },
-    fullName: { type: String },
-    phoneNumber: { type: String },
-    region: { type: String }, // New field for region
-    district: { type: String }, // New field for district
-    registrationType: { type: String },
-    pickupSchedule: { type: String },
-    // You can add any other fields required by your form
+const ServiceSchema = new mongoose.Schema({
+  companyName: { type: String, required: true }, // Full name of the user
+  fullName: { type: String, required: true }, // Full name of the user
+  phoneNumber: { type: String, required: true }, // User's phone number
+  region: { type: String, required: true }, // User's selected region
+  district: { type: String, required: true }, // User's selected district
+  registrationType: { type: String, required: true }, // User's selected registration type
+  pickupSchedule: { type: String, required: true }, // User's selected pickup schedule
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the user
+  createdAt: {
+    type: Date,
+    default: Date.now, // Timestamp of when the service was created
   },
-  { timestamps: true }
-);
+});
 
-const Services = mongoose.model("Services", ServicesSchema);
+const Services = mongoose.model("Service", ServiceSchema); // Change model name to 'Service'
 
 module.exports = Services;
